804661660069bdb7453396a4df471bc883cc7111
Vira Dovhalska <139339635+virpol@users.noreply.github.com>
2023-10-19 00:03:14 +0300

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    weather
+      <h1> Weather</h1>
+      
+      <footer>
+        This project was coded by Vira Dovhalska and is {" "}
+        <a
+          href="https://github.com/virpol/my-weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+           open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
