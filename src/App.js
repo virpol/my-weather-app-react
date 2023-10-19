4e0b7cf5849488a3bdc1c56a42bd91c6df31fdc6
Vira Dovhalska <139339635+virpol@users.noreply.github.com>
2023-10-19 12:08:38 +0300

    Added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Weather</h1>
-      
-      <footer>
-        This project was coded by Vira Dovhalska and is {" "}
-        <a
-          href="https://github.com/virpol/my-weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-           open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather</h1>
+
+        <footer>
+          This project was coded by Vira Dovhalska and is{" "}
+          <a
+            href="https://github.com/virpol/my-weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
